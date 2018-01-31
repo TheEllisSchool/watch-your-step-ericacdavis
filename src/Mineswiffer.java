f4a2057f3b947afd7fde839fccca92429a892ece
ericadavis <ericadavis@172.16.21.51>
2018-01-31 13:05:35 -0500

    done!

--- src/Mineswiffer.java
+++ src/Mineswiffer.java
@@ -1,16 +1,29 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class Mineswiffer extends JFrame{
+	private static final int GRIDSIZE = 10;
+	private static final int NUMBEROFHOLES = 10;
+	
+	private TerrainButton[][] terrain = new TerrainButton[GRIDSIZE] [GRIDSIZE];
+	private int totalRevealed = 0;
+	
 public Mineswiffer() {
 	initGUI();
+	setHoles();
 	setTitle("Mineswiffer Game     Creator Erica Davis");
 	setSize(200, 100); //pixels
 	setResizable(true);
@@ -33,6 +46,122 @@ private void initGUI() {
 	Font titleFont = new Font("Times New Roman", Font.BOLD, 50);
 	titleLabel.setForeground(new Color(83, 22, 226));
 	titleLabel.setFont(titleFont);
+	
+	//center panel
+	JPanel centerPanel = new JPanel();
+	centerPanel.setLayout(new GridLayout (GRIDSIZE, GRIDSIZE));
+	add(centerPanel, BorderLayout.CENTER);
+	for (int r = 0; r< GRIDSIZE; r++) {
+		for (int c = 0; c < GRIDSIZE; c++) {
+		terrain[r][c] = new TerrainButton (r,c);
+		terrain[r][c].addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				TerrainButton button = (TerrainButton) e.getSource();
+				int row = button.getRow();
+				int col = button.getCol();
+				clickedTerrain(row,col);
+			}
+		});
+		centerPanel.add(terrain [r][c]);
+		}
+	}
+}
+private void clickedTerrain(int row, int col) {
+	if (terrain[row][col].hasHole()) {
+		String message = "Game over. Would you like to play again?";
+		promptForNewGame(message);
+	}
+	else {
+	check(row,col);
+	checkNeighbors(row,col);
+	if (GRIDSIZE * GRIDSIZE -NUMBEROFHOLES == totalRevealed) {
+		String message = "'You won! Would you like to play again?";
+		promptForNewGame(message);
+	}
+	}
+}
+private void promptForNewGame(String message) {
+	showHoles();
+	int option = JOptionPane.showConfirmDialog(this, message, "Play Again?", JOptionPane.YES_NO_OPTION);
+	if(option == JOptionPane.YES_OPTION) {
+		newGame();
+	}else {
+		System.exit(0);
+	}
+}
+private void showHoles() {
+	for(int r = 0; r< GRIDSIZE; r++) {
+		for(int c = 0; c < GRIDSIZE; c++) {
+			if(terrain[r][c].hasHole()) {
+				terrain[r][c].reveal(true);
+			}
+		}
+	}
+}
+private void newGame() {
+	for (int r = 0; r< GRIDSIZE; r++) {
+		for (int c = 0; c < GRIDSIZE; c++) {
+			terrain[r][c].reset();
+		}
+		setHoles();
+		totalRevealed= 0;
+	}
+}
+private void check(int row, int col) {
+	if(row > -1 && row < GRIDSIZE && col > -1 && col < GRIDSIZE 
+		&&! terrain[row][col].hasHole()
+		&&! terrain[row] [col].isRevealed() ){
+			terrain[row][col].reveal(true);
+			totalRevealed++;
+			if(!terrain[row][col].isNextToHoles()) {
+				checkNeighbors(row,col);
+			}
+		}
+	}
+private void checkNeighbors(int row, int col) {
+	check(row -1, col-1);
+	check(row -1, col );
+	check(row -1, col+1 );
+	check(row , col -1);
+	check(row , col+1 );
+	check(row +1, col -1 );
+	check(row +1, col );
+	check(row +1, col+1 );
+}
+
+private void setHoles() {
+	Random rand = new Random();
+	int pickRow;
+	int pickCol;
+	for (int i =0; i < NUMBEROFHOLES; i++) {
+		do {
+			pickRow = rand.nextInt(GRIDSIZE);
+			pickCol = rand.nextInt(GRIDSIZE);
+		}while (terrain[pickRow][pickCol].hasHole());
+		terrain[pickRow][pickCol].setHole(true);
+		addToNeighborsHoleCount(pickRow, pickCol);
+		//terrain[pickRow][pickCol].reveal(true);
+
+	}
+	
+}
+private void addToNeighborsHoleCount(int row, int col){
+	//fix below
+addToHoleCount(row -1, col);
+addToHoleCount(row-1, col -1 );
+addToHoleCount(row +1, col -1 );
+addToHoleCount(row, col-1 );
+addToHoleCount(row+1, col );
+addToHoleCount(row-1, col+1);
+addToHoleCount(row, col +1);
+addToHoleCount(row+1, col+1);
+}
+private void addToHoleCount(int row, int col){
+	if (row > -1 && row < GRIDSIZE && col > -1 && col < GRIDSIZE) {
+		terrain[row][col].increaseHoleCount(); 
+		//terrain[row][col].reveal(true); 
+	}
 }
 
 public static void main(String[] args) {
--- src/TerrainButton.java
+++ src/TerrainButton.java
@@ -1,9 +1,11 @@
+import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JButton;
 
 public class TerrainButton extends JButton{
 private static final int SIZE = 50;
+private static final int GRIDSIZE = 0;
 private int row = 0;
 private int col = 0;
 private int nextToHoles = 0;
@@ -12,8 +14,8 @@ private boolean revealed = false;
 
 
 public TerrainButton (int r, int c) {
-	c = col;
-	r = row;
+	col = c;
+	row = r;
 	setSize(SIZE,SIZE);
 	Dimension size = new Dimension (SIZE,SIZE);
 	setPreferredSize(size);
@@ -37,11 +39,38 @@ public void increaseHoleCount() {
 	nextToHoles+=1;
 }
 public boolean isNextToHoles() {
-	if (nextToHoles>=0) {
+	if (nextToHoles>0) {
 		return true;
 	}
 	else {
 		return false;
 	}
+}
+
+public void reveal(boolean reveal) {
+	revealed = reveal;
+	if (revealed = true) {
+		if (hasHole()) {
+			setBackground(Color.black);
+		}
+		if (! hasHole()) {
+			setBackground(Color.cyan);
+			if (nextToHoles>0) {
+				setText("" + nextToHoles);
+			}
+		}
+	else {
+		setBackground(null);
+		setText("");
+	}
+		setFocusPainted(false);
+	}
+}
+public void reset() {
+	hole= false;
+	revealed = false;
+	nextToHoles = 0;
+	setText("");
+	setBackground(null);
 }
 }
